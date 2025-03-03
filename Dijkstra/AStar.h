562aa631444642447622a288fdfa32951170fd1e
Claire Benes <claire.benes@artfx.fr>
2025-03-03 15:13:11 +0100

    [chore] code cleaning

--- Dijkstra/AStar.h
+++ Dijkstra/AStar.h
@@ -8,7 +8,9 @@ class AStar
 {
 public:
 	void Algorithm(const std::vector<std::vector<float>>& grid, Vector2 start, Vector2 goal);
+
 	std::vector<Node*> GetChildrens(Node* current, const std::vector<std::vector<float>>& grid);
+
 	float Heuristic(Node* a, Node* b);
 	void CleanupNodes(std::vector<Node*>& nodes);
 };
